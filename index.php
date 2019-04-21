30a43a4cc147eb0bb005b3670c8e95700028ee64
Felipe Burbano <felipeburbano16@gmail.com>
2019-04-21 18:56:49 -0500

    test2

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 header('Location: /landing/index.html');
-header('Location: /reservas/clientes.html');
-header('Location: /socios/empresas.html');
+header('Location: /reservas_clientes/clientes.html');
+header('Location: /socios_empresas/empresas.html');
 ?>
